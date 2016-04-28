80bcc69653cfc479686a2bf8b3c600128f4d2971
Christian Roring <croring@Christians-MacBook-Pro.local>
2016-04-28 10:03:47 +0200

    Changed the wave generator to make it more flexibel

--- lib/WaveGenerator/src/WaveGenerator.cpp
+++ lib/WaveGenerator/src/WaveGenerator.cpp
@@ -9,78 +9,71 @@
 #include "Arduino.h"
 #include "WaveGenerator.h"
 
-WaveGenerator::WaveGenerator(char _wavePin, char _ratePin)
+WaveGenerator::WaveGenerator(unsigned int _bpm, unsigned int *_depth, int *_wave, float *_multi, int *_mod)
 {
-    wavePin = _wavePin;
-    ratePin = _ratePin;
+    depth = _depth;
+    wave = _wave;
+    multi = _multi;
+    mod = _mod;
+    lfo = 8;
 
-    pinMode(wavePin, OUTPUT);
-    pinMode(ratePin, OUTPUT);
-
-    orgPeriod = BPM_2_PERIOD(120);
-    period = orgPeriod;
+    period = BPM_2_PERIOD(120);
+    periodMultiplied = period * *multi;
 
     firstPeriod = millis();
-    halfPeriod = firstPeriod / 2;
 
     waveFn[0] = &WaveGenerator::waveSin;
     waveFn[1] = &WaveGenerator::waveSquare;
     waveFn[2] = &WaveGenerator::waveTri;
-    waveFn[3] = &WaveGenerator::waveRampUp;
-    waveFn[4] = &WaveGenerator::waveRampDown;
-};
+    waveFn[3] = &WaveGenerator::waveSaw;
+    waveFn[4] = &WaveGenerator::waveReverseSaw;
 
-void WaveGenerator::setParams(unsigned int *_bpm, unsigned int *_depth, int *_wave, float *_mult, int *_mod, unsigned int *_lfo)
-{
-    bpm = _bpm;
-    depth = _depth;
-    wave = _wave;
-    mult = _mult;
-    mod = _mod;
-    lfo = _lfo;
-
-    updatePeriod(BPM_2_PERIOD(*bpm));
-};
-
-void WaveGenerator::generate()
-{
-    currentMillis = millis();
-    int offset = (currentMillis - firstPeriod) % period;
-
-    ldrWriteIfChanged((this->*waveFn[*wave])(offset));
+    updatePeriod(BPM_2_PERIOD(_bpm));
 };
 
 void WaveGenerator::updatePeriod(int _newVal)
 {
-    orgPeriod = period;
-    int periodMultiplied = _newVal * *mult;
+    period = _newVal;
+
+    int newPeriodMultiplied = _newVal * *multi;
 
     currentMillis = millis();
     unsigned int _offset = (currentMillis - firstPeriod) % periodMultiplied;
 
     //adjust first period so that we are the same % of the way through the period
     //so there aren't any jarring transitions
-    int effectiveNewOffset = map(_offset, 0, orgPeriod - 1, 0, periodMultiplied - 1);
+    int effectiveNewOffset = map(_offset, 0, periodMultiplied - 1, 0, newPeriodMultiplied - 1);
     firstPeriod = currentMillis - effectiveNewOffset;
 
-    halfPeriod = periodMultiplied / 2;
+    halfMultipliedPeriod = newPeriodMultiplied / 2;
 
-    period = periodMultiplied;
+    periodMultiplied = newPeriodMultiplied;
 };
 
-// //actually cos so we're on at period start
+int WaveGenerator::generate()
+{
+    currentMillis = millis();
+    int offset = (currentMillis - firstPeriod) % periodMultiplied;
+
+    float modulation = 0.0;
+    if (*mod > 0.0) modulation = generateLFO();
+
+    return constrain((this->*waveFn[*wave])(offset) + modulation, 0, 255);
+};
+
+//Private functions
+
+// actually cos so we're on at period start
 int WaveGenerator::waveSin(unsigned int _offset)
 {
-    float rads = ((float)_offset / (float)period) * TWO_PI;
+    float rads = ((float)_offset / (float)periodMultiplied) * TWO_PI;
 
-    int temp = constrain((cos(rads) + 1.0) * (255 - *depth) / 2.0 + *depth, 0, 255); //cap at 255 instead of 256
-
-    return temp;
+    return constrain((cos(rads) + 1.0) * (255 - *depth) / 2.0 + *depth, 0, 255); //cap at 255 instead of 256
 }
 
 int WaveGenerator::waveSquare(unsigned int _offset)
 {
-    if(_offset < halfPeriod) {
+    if(_offset < halfMultipliedPeriod) {
         return 255;
     }
     else {
@@ -90,46 +83,30 @@ int WaveGenerator::waveSquare(unsigned int _offset)
 
 int WaveGenerator::waveTri(unsigned int _offset)
 {
-    if(_offset < halfPeriod)
+    if(_offset < halfMultipliedPeriod)
     {
-        return map(_offset, 0, halfPeriod, 255, *depth);
+        return map(_offset, 0, halfMultipliedPeriod, 255, *depth);
     }
     else
     {
-        return map(_offset, halfPeriod + 1, period - 1, *depth, 255);
+        return map(_offset, halfMultipliedPeriod + 1, periodMultiplied - 1, *depth, 255);
     }
 }
 
-int WaveGenerator::waveRampUp(unsigned int _offset)
+int WaveGenerator::waveSaw(unsigned int _offset)
 {
-    return map(_offset, 0, period - 1, *depth, 255);
+    return map(_offset, 0, periodMultiplied - 1, *depth, 255);
 }
 
-int WaveGenerator::waveRampDown(unsigned int _offset)
+int WaveGenerator::waveReverseSaw(unsigned int _offset)
 {
-  return map(_offset, 0, period - 1, 255, *depth);
+  return map(_offset, 0, periodMultiplied - 1, 255, *depth);
 }
 
 float WaveGenerator::generateLFO()
 {
-    unsigned int modOffset = (currentMillis - firstPeriod) % (orgPeriod * *lfo);
+    unsigned int modOffset = (currentMillis - firstPeriod) % (period * lfo);
     // generate the mod rad
-    float modRads = ((float)modOffset / (float)(orgPeriod * *lfo)) * TWO_PI;
+    float modRads = ((float)modOffset / (float)(period * lfo)) * TWO_PI;
     return cos(modRads + 11.125) * (*mod * 2) / 2.0; // magic numbers :)
-}
-
-// writing the ldr
-void WaveGenerator::ldrWriteIfChanged(int _newValue)
-{
-    float modulation = 0.0;
-    if (*mod > 0) modulation = generateLFO();
-
-    int newLDRValue = constrain(_newValue + modulation, 0, 255);
-
-    // if(lastLDRValue != newLDRValue)
-    // {
-        analogWrite(wavePin, newLDRValue);
-        analogWrite(ratePin, newLDRValue);
-        lastLDRValue = newLDRValue;
-    // }
 }
--- lib/WaveGenerator/src/WaveGenerator.h
+++ lib/WaveGenerator/src/WaveGenerator.h
@@ -18,55 +18,44 @@ class WaveGenerator
 public:
      /**
      Initialization function
-     @param char wavePin The output pin for the vactrol.
-     @param char ratePin The output pin for rate led.
-     */
-     WaveGenerator(char, char);
-     /**
-     Links the parameters in the generation
-     @param unsigned int* *_bpm Pointer to the bpm.
+     @param unsigned int _bpm The BPM value.
      @param unsigned int* _depth Pointer to the depth.
      @param int* _wave Pointer to the wave type.
-     @param float* _mult Pointer to the multiplier.
+     @param float* _multi Pointer to the multiplier.
      @param int* _mod Pointer to the modulation.
-     @param unsigned int* _lfo Pointer to the lfo devision of the main period.
      */
-     void setParams(unsigned int*, unsigned int*, int*, float*, int*, unsigned int*);
-     /**
-    Generates the waveform for the wave led (vactrol) and a status led
-     */
-     void generate();
-private:
-    char wavePin;
-    char ratePin;
+     WaveGenerator(unsigned int, unsigned int*, int*, float*, int*);
 
-    unsigned int *bpm;
+     /**
+     Updates the internal period to incorporate the old en new offset
+     */
+     void updatePeriod(int);
+     /**
+     Generates the waveform for the wave led (vactrol) and a status led
+     */
+     int generate();
+private:
     unsigned int *depth;
     int *wave;
-    float *mult;
+    float *multi;
     int *mod;
-    unsigned int *lfo;
+    unsigned int lfo;
 
-    int orgPeriod;
     int period;
+    int periodMultiplied;
     unsigned long firstPeriod;
-    unsigned int halfPeriod;
+    unsigned int halfMultipliedPeriod;
     unsigned long currentMillis;
-
-    int lastLDRValue;
-
-    void updatePeriod(int);
 
     int waveSin(unsigned int);
     int waveSquare(unsigned int);
     int waveTri(unsigned int);
-    int waveRampUp(unsigned int);
-    int waveRampDown(unsigned int);
+    int waveSaw(unsigned int);
+    int waveReverseSaw(unsigned int);
 
     int (WaveGenerator::*waveFn[5])(unsigned int);
 
     float generateLFO();
-    void ldrWriteIfChanged(int);
 };
 
 #endif
