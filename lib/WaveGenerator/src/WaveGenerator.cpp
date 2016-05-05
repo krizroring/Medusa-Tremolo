415f5ea5b6480c25d0010ad256622f2efb3d2350
Christian Roring <croring@Christians-MacBook-Pro.local>
2016-05-05 14:42:03 +0200

    Changed the wavegenerator to control the ldr completely

--- lib/WaveGenerator/src/WaveGenerator.cpp
+++ lib/WaveGenerator/src/WaveGenerator.cpp
@@ -8,14 +8,18 @@
 
 #include "Arduino.h"
 #include "WaveGenerator.h"
+#include "MedusaDisplay.h"
 
-WaveGenerator::WaveGenerator(unsigned int _bpm, unsigned int *_depth, int _wave, float _multi, int *_mod) {
+static const float multiplier_table[] PROGMEM =  { 0.25, 0.5, 1, 2, 4};
+
+WaveGenerator::WaveGenerator(int _bpm, int _depth, int _wave, int _multi, int _mod) {
     depth = _depth;
     wave = _wave;
     multi = _multi;
     mod = _mod;
     lfo = 8;
 
+    multiplier = pgm_read_float(multiplier_table+multi);
     firstPeriod = millis();
 
     waveFn[0] = &WaveGenerator::waveSin;
@@ -24,16 +28,67 @@ WaveGenerator::WaveGenerator(unsigned int _bpm, unsigned int *_depth, int _wave,
     waveFn[3] = &WaveGenerator::waveSaw;
     waveFn[4] = &WaveGenerator::waveReverseSaw;
 
+    updateDepth(0);
     updateBPM(_bpm);
 };
 
-void WaveGenerator::updateBPM(int _bpm) {
-    bpm = _bpm;
-    int _period = BPM_2_PERIOD(_bpm);
-    period = _period;
+int WaveGenerator::generate() {
+    currentMillis = millis();
+    int offset = (currentMillis - firstPeriod) % periodMultiplied;
 
-    int newPeriodMultiplied = _period * multi;
+    float modulation = 0.0;
+    if (mod > 0.0) modulation = generateLFO();
 
+    return constrain((this->*waveFn[wave])(offset) + modulation, MAX_LDR_DEPTH, MIN_LDR_DEPTH);
+};
+
+void WaveGenerator::setBPM(int _bpm) {
+    period = BPM_2_PERIOD(_bpm);
+    newPeriodMultiplied = period * multiplier;
+
+    updatePeriod();
+};
+
+int WaveGenerator::updateBPM(int _modifier) {
+    bpm += _modifier;
+    bpm = constrain(bpm, MIN_BPM, MAX_BPM);
+    setBPM(bpm);
+    return bpm;
+}
+
+int WaveGenerator::updateDepth(int _modifier) {
+    depth += _modifier;
+    depth = constrain(depth, MAX_DEPTH, MIN_DEPTH);
+
+    ldrDepth = map(depth, MIN_DEPTH, MAX_DEPTH, MAX_LDR_DEPTH, MIN_LDR_DEPTH);
+
+    return depth;
+}
+int WaveGenerator::updateWave(int _modifier) {
+    wave += _modifier;
+    wave = constrain(wave, MIN_WAVE, MAX_WAVE);
+    updatePeriod();
+
+    return wave;
+}
+int WaveGenerator::updateMultiplier(int _modifier) {
+    multi += _modifier;
+    multi = constrain(multi, MIN_MULTI, MAX_MULTI);
+    multiplier = pgm_read_float(multiplier_table+multi);
+    updatePeriod();
+
+    return multi;
+}
+int WaveGenerator::updateModulation(int _modifier) {
+    mod += _modifier;
+    mod = constrain(mod, MIN_MOD, MAX_MOD);
+
+    return mod;
+}
+
+//Private functions
+
+void WaveGenerator::updatePeriod() {
     currentMillis = millis();
     unsigned int _offset = (currentMillis - firstPeriod) % periodMultiplied;
 
@@ -45,64 +100,41 @@ void WaveGenerator::updateBPM(int _bpm) {
     halfMultipliedPeriod = newPeriodMultiplied / 2;
 
     periodMultiplied = newPeriodMultiplied;
-};
-
-void WaveGenerator::updateWave(int _wave) {
-    wave = _wave;
-    updateBPM(bpm);
-};
-
-void WaveGenerator::updateMultiplier(float _multi) {
-    multi = _multi;
-    updateBPM(bpm);
 }
-
-int WaveGenerator::generate() {
-    currentMillis = millis();
-    int offset = (currentMillis - firstPeriod) % periodMultiplied;
-
-    float modulation = 0.0;
-    if (*mod > 0.0) modulation = generateLFO();
-
-    return constrain((this->*waveFn[wave])(offset) + modulation, 0, 255);
-};
-
-//Private functions
-
 // actually cos so we're on at period start
 int WaveGenerator::waveSin(unsigned int _offset) {
     float rads = ((float)_offset / (float)periodMultiplied) * TWO_PI;
 
-    return constrain((cos(rads) + 1.0) * (255 - *depth) / 2.0 + *depth, 0, 255); //cap at 255 instead of 256
+    return constrain((cos(rads) + 1.0) * (MIN_LDR_DEPTH - ldrDepth) / 2.0 + ldrDepth, MAX_LDR_DEPTH, MIN_LDR_DEPTH); //cap at 255 instead of 256
 }
 
 int WaveGenerator::waveSquare(unsigned int _offset) {
     if(_offset < halfMultipliedPeriod) {
-        return 255;
+        return MIN_LDR_DEPTH;
     } else {
-        return *depth;
+        return ldrDepth;
     }
 }
 
 int WaveGenerator::waveTri(unsigned int _offset) {
     if(_offset < halfMultipliedPeriod) {
-        return map(_offset, 0, halfMultipliedPeriod, 255, *depth);
+        return map(_offset, 0, halfMultipliedPeriod, MIN_LDR_DEPTH, ldrDepth);
     } else {
-        return map(_offset, halfMultipliedPeriod + 1, periodMultiplied - 1, *depth, 255);
+        return map(_offset, halfMultipliedPeriod + 1, periodMultiplied - 1, ldrDepth, MIN_LDR_DEPTH);
     }
 }
 
 int WaveGenerator::waveSaw(unsigned int _offset) {
-    return map(_offset, 0, periodMultiplied - 1, *depth, 255);
+    return map(_offset, 0, periodMultiplied - 1, ldrDepth, MIN_LDR_DEPTH);
 }
 
 int WaveGenerator::waveReverseSaw(unsigned int _offset) {
-  return map(_offset, 0, periodMultiplied - 1, 255, *depth);
+  return map(_offset, 0, periodMultiplied - 1, MIN_LDR_DEPTH, ldrDepth);
 }
 
 float WaveGenerator::generateLFO() {
     unsigned int modOffset = (currentMillis - firstPeriod) % (period * lfo);
     // generate the mod rad
     float modRads = ((float)modOffset / (float)(period * lfo)) * TWO_PI;
-    return cos(modRads + 11.125) * (*mod * 2) / 2.0; // magic numbers :)
+    return cos(modRads + 11.125) * (mod * 2) / 2.0; // magic numbers :)
 }
--- lib/WaveGenerator/src/WaveGenerator.h
+++ lib/WaveGenerator/src/WaveGenerator.h
@@ -10,51 +10,83 @@
 #define WAVE_GENERATOR_H
 
 #include "Arduino.h"
+#include "MedusaDisplay.h"
 
 #define BPM_2_PERIOD(bpm)(60000/bpm)
+
+#define MIN_BPM 60
+#define MAX_BPM 240
+#define MIN_DEPTH 100
+#define MAX_DEPTH 0
+#define MIN_WAVE 0
+#define MAX_WAVE 4
+#define MIN_MOD 0
+#define MAX_MOD 10
+#define MIN_MULTI 0
+#define MAX_MULTI 5
+
+#define MIN_LDR_DEPTH 255
+#define MAX_LDR_DEPTH 0
 
 class WaveGenerator
 {
 public:
      /**
      Initialization function
-     @param unsigned int _bpm The BPM value.
-     @param unsigned int* _depth Pointer to the depth.
+     @param int _bpm The BPM value.
+     @param int _depth The depth value.
      @param int _wave Pointer to the wave type.
-     @param float _multi Pointer to the multiplier.
-     @param int* _mod Pointer to the modulation.
+     @param int _multi The multiplier.
+     @param int _mod The modulation value.
      */
-     WaveGenerator(unsigned int, unsigned int*, int, float, int*);
+     WaveGenerator(int, int, int, int, int);
 
      /**
      Updates the bpm
      */
-     void updateBPM(int);
-     /**
-     Updates the wave form
-     */
-     void updateWave(int);
-     /**
-     Updates the bpm
-     */
-     void updateMultiplier(float);
+     void setBPM(int);
      /**
      Generates the waveform for the wave led (vactrol) and a status led
      */
      int generate();
+     /**
+     Increases or decreases the BPM
+     */
+     int updateBPM(int);
+     /**
+     Increases or decreases the Depth
+     */
+     int updateDepth(int);
+     /**
+     Increases or decreases the wave type
+     */
+     int updateWave(int);
+     /**
+     Increases or decreases the multiplier
+     */
+     int updateMultiplier(int);
+     /**
+     Increases or decreases the modulation
+     */
+     int updateModulation(int);
 protected:
     int bpm;
-    unsigned int *depth;
+    int depth;
     int wave;
-    float multi;
-    int *mod;
+    int multi;
+    int mod;
     unsigned int lfo;
 
+    int ldrDepth;
+    float multiplier;
     int period;
     int periodMultiplied;
+    int newPeriodMultiplied;
     unsigned long firstPeriod;
     unsigned int halfMultipliedPeriod;
     unsigned long currentMillis;
+
+    void updatePeriod();
 
     int waveSin(unsigned int);
     int waveSquare(unsigned int);
